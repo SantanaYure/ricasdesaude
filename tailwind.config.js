15ba55843d200c5f2111a46e6e101533ff373efa
santanaYure <yure.s.santana@outlook.com>
2025-11-27 02:47:12 -0300

    style: atualiza cores no arquivo de configuração do Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        'dark-900': '#111111',
-        'dark-800': '#181818',
-        'dark-700': '#1f1f1f',
-        'primary': '#ec4899',
-        'secondary': '#f97316',
-        'feedback-success': '#22c55e',
-        'feedback-error': '#ef4444',
+        'dark-900': '#0a0a0a', // fundo principal
+        'dark-800': '#1a1a1a', // fundo secundário/cartões
+        'primary-pink': '#ec4899', // rosa vibrante
+        'secondary-orange': '#f97316', // laranja parceiro
+        'feedback-green': '#22c55e', // verde feedback
+        'feedback-red': '#ef4444', // vermelho feedback
+        'gray-400': '#e5e5e5', // texto secundário
+        'gray-500': '#a3a3a3', // texto secundário
       },
     },
   },
